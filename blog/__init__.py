a766fb2cc3261cc34bf2a676ce719d180d7991d9
aderules <deolasalako@gmail.com>
2016-04-04 04:08:23 +0000

    fixed travis issues

--- blog/__init__.py
+++ blog/__init__.py
@@ -5,7 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 config_path = os.environ.get("CONFIG_PATH", "blog.config.DevelopmentConfig")
 if os.environ.get("TRAVIS"):
-    # CJA 20160403: Travis doesn't set env vars properly.
+    # CJA 20160403: Travicd ..s doesn't set env vars properly.
     config_path = "blog.config.TravisConfig"
 
 
